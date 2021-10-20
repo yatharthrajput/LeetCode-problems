b7a1205cbdfb7d8bb6a6a61d21a1180a88ba0413
Yatharth Rajput <66031538+yatharthrajput@users.noreply.github.com>
2021-10-20 13:52:07 +0530

    Add files via upload

---
+++ time_comlexity1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+
+    int T,N;
+    cin>>T;
+    for(int i=0;i<T;i++){cin>>N;}
+    cout<<N<<endl;
+
+    // YOUR CODE GOES HERE
+    // Please take input and print output to standard input/output (stdin/stdout)
+    // E.g. 'cin' for input & 'cout' for output
+
+
+    return 0;
+}
