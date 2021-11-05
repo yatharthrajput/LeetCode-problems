6c7dc37334ed774d1ac8d92ae280051dfe49fcb2
Yatharth Rajput <66031538+yatharthrajput@users.noreply.github.com>
2021-11-05 17:26:39 +0530

    Add files via upload

---
+++ removeDuplicates.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public:
+        int removeDuplicates(vector<int>& nums) {
+            int count = 0;
+            int n=sizeof(nums)/sizeof(nums[0]);
+    for(int i = 1; i < n; i++){
+        if(nums[i] == nums[i-1]) count++;
+        else nums[i-count] = nums[i];
+    }
+    return n-count;
+        }
+    };
